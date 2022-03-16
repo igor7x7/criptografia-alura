3b191a8d6bc00b190c45b1c8787e023533b33c77
igor7x7 <80541299+igor7x7@users.noreply.github.com>
2022-03-15 21:18:44 -0300

    Update app.js

--- app.js
+++ app.js
@@ -30,3 +30,35 @@ buttonCript.addEventListener('click', () => {
 let texto = cripto.innerText
 
 console.log(texto)
+
+/*
+*********modelo para testar**********
+
+const texto = "eu estou com saudades"
+let cript = texto.split('')
+let i = 0
+
+for(let i = 0; i<= texto.length; i++){
+  if(cript[i] == 'a'){
+    cript[i] = 'atr'
+  }
+  if(cript[i] == 'e'){
+    cript[i] = 'eqb'
+  }
+  if(cript[i] == 'i'){
+    cript[i] = 'ihr'
+  }
+  if(cript[i] == 'o'){
+    cript[i] = 'opbr'
+  }
+  if(cript[i] == 'u'){
+    cript[i] = 'udyt'
+  }
+}
+
+let textoCript = cript.toString()
+let reform = textoCript.replace(/,/g,"")
+
+console.log(cript)
+console.log(textoCript)
+console.log(reform) */
